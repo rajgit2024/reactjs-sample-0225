22a0ad36da3f70d4aa51b96869e7d41786af5b37
raj dubey <rajdu590@gmail.com>
2025-06-04 23:00:38 +0530

    ENV updation

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const userRoute=require("./routes/userRoute")
 require("dotenv").config();
 
 const app=express();
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 app.use("/api/tasks",taskRoute);
